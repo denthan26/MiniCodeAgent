86b9ef54737649cf5e1a029359f736a761e1cd44
denthan26 <78246711+denthan26@users.noreply.github.com>
2026-05-25 23:09:25 +0800

    apikeyreset

--- Apikey.py
+++ Apikey.py
@@ -1,6 +1,6 @@
 class key:
     def __init__(self):
-        self.key = "f7e4af91d46e4e3388d3c181b7c1905b.uhL2jVeRYuEHLgoz"
+        self.key = "your LLM api key"
     
     def get_key(self):
         return self.key
